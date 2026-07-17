c4d0add25f3bc76a689b7f5a7533599a64133ce9
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-17 11:08:44 +0300

    Create pos-category.js

---
+++ assets/js/pos/pos-category.js
@@ -0,0 +1,18 @@
+/*
+==========================================================
+PAPPRITO ERP
+Module : Point of Sale (POS)
+File   : assets/js/pos/pos-category.js
+Description : Load Categories from Firebase
+==========================================================
+*/
+
+// ==========================================================
+// LOAD CATEGORY LIST
+// ==========================================================
+
+function loadPOSCategories() {
+
+    // Firebase loading code will be added in the next step.
+
+}
