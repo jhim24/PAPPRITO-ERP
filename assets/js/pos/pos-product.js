d0e64f52d44408901d5399daba6685920417e46c
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-17 11:46:55 +0300

    Update pos-product.js

--- assets/js/pos/pos-product.js
+++ assets/js/pos/pos-product.js
@@ -1,1 +1,36 @@
+/*
+==========================================================
+PAPPRITO ERP
+Module : Point of Sale (POS)
+File   : assets/js/pos/pos-product.js
+Description : Load Products from Firebase
+==========================================================
+*/
 
+// ==========================================================
+// LOAD ALL PRODUCTS
+// ==========================================================
+
+function loadPOSProducts() {
+
+    const productGrid = document.getElementById("productGrid");
+
+    productGrid.innerHTML = "";
+
+    // ==========================================================
+    // GET ACTIVE PRODUCTS FROM FIREBASE
+    // ==========================================================
+
+    firebase.database()
+        .ref("products")
+        .orderByChild("status")
+        .equalTo("Active")
+        .on("value", function(snapshot){
+
+            productGrid.innerHTML = "";
+
+            // Products will be rendered in STEP 15
+
+        });
+
+}
